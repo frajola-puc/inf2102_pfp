9720df9b19dbef2ce7b67a4dda201e3f47c34917
Frajola <frajola.puc@gmail.com>
2023-02-08 09:45:25 -0300

    OK: Main.

--- VRP/main.jl
+++ VRP/main.jl
@@ -16,7 +16,33 @@ function main(V_S_ARG...)
 		exit()
 	end
 	println("Depois...")
+	#VRP.greet()
+
+
+	println("Depois...")
 	VRP.greet()
+
+	"""
+	for i = [2 : length(V_S_ARG): 2]
+		local sArg_ = lowercase(V_S_ARG[i])
+
+		if (sArg_ == "-i")
+			oArgument.sInputFile = V_S_ARG[i+1]
+			if (oArgument.sInputFile === "" || match(r".+\.json", oArgument.sInputFile) === nothing)
+				throw("Unexpected file:"+oArgument.sInputFile)
+		elseif (sArg_ == "-s")
+			arguments.iSeed = parse(Int64, V_S_ARG[i+1])
+		elseif (sArg_ == "-t")
+			oArgument.fExecutionTime = parse(Int64, V_S_ARG[i+1])
+		elseif (sArg_ == "-k")
+			oArgument.iNearestK = parse(Int64, V_S_ARG[i+1])
+		else
+			throw("Unexpected argument:"+sArg_)
+		end
+	end
+
+	VRP(oArgument)
+	"""	
 
 end
 
